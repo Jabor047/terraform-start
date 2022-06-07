7043f830bed03a33a6c099fa533721091fd85acf
jabor047 <gkkarobia@gmail.com>
2022-06-07 15:46:20 +0100

    Created the resources to deploy a VPC on GVP

---
+++ learn-terraform-gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        google  = {
+            source = "hashicorp/google"
+            version = "~> 3.5.0"
+        }
+    }
+}
+
+provider "google" {
+    credentials = file("~/Projects/mlflow-deployment-gcp/key.json")
+
+    project = var.project_id
+    region = var.region
+    zone = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+    name = "terraform-network"
+}
