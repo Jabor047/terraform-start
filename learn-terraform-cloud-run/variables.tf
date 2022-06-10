0dc160fb149e155e2eb62dea76fd8034016a316d
Jabor047 <gkkarobia@gmail.com>
2022-06-10 15:07:05 +0100

    created the required variables to run cloud run

---
+++ learn-terraform-cloud-run/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The Google Cloud project ID in which to provision the resources."
+  type = string
+}
+
+variable "region" {
+  description = "The Google Cloud region in which to provision the resources."
+  type = string
+}
+
+variable "zone" {
+  description = "The Google Cloud zone in which to provision the resources."
+  type = string
+}
